a03cd1d88df69b7b84a05b5a18462f0a17b77c9a
ナナシーナ <vitavita3363@yahoo.co.jp>
2019-02-01 11:23:48 +0900

    日付

--- routes/index.js
+++ routes/index.js
@@ -132,7 +132,7 @@ function returnMsg(url){
                             },
                             {
                                 "type": "text",
-                                "text": moment('hours', 9).format("MM月DD日HH時mm分"),
+                                "text": moment().add('H', 9).format("MM月DD日HH時mm分"),
                                 
                                 "wrap": true,
                                 "color": "#000000",
